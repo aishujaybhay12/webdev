de6d66f78e93a537fe6f8e59afd0a2a80e7ff9df
aishujaybhay12 <aishwaryajaybhay203@gmail.com>
2024-11-14 16:49:17 +0530

    Add files via upload

---
+++ py 1.py
@@ -0,0 +1,24 @@
+name = 'aishwarya jaybhay'
+
+print(name.find("s"))
+print(name.find("y"))
+print(name.count("y"))
+print(name.count("s"))
+print(name.count("a"))
+print(name.count("s"))
+print(name.replace("s", "w"))
+
+print(name.replace("aishwarya ", "aishu"))
+
+print("p" in name)
+
+print("aishwarya" in name)
+print(45*69)
+print(45%69)
+
+i=69
+i+=1
+print(i)
+ # is used for comment
+
+print(3>6)
